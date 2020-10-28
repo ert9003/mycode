8f17f0a5dd7939524522c2f3454b88b48b48fa29
ert9003 <ert9003@nyp.org>
2020-10-28 16:31:34 +0000

    Learning the basics of deploying with Ansible

---
+++ pychallenge01/pychallenge01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+dogs = []
+fish = []
+
+pets = [{"type": "dog", "name": "mudge", "age": 4},
+        {"type": "fish", "name": "dorothy", "age": "3 days"},
+        {"type": "dog", "name": "bingo", "age": 12},
+        {"type": "fish", "name": "nemo", "age": 2},
+        {"type": "cat", "name": "fluffy", "age": 1}
+
